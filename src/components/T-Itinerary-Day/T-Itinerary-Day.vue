edb10687dcca45e902b603ef61a97fbaaf447c24
Jóhan Dam-Davidsen <johan.davidsen@fjakkarin.com>
2024-09-27 16:46:27 +0100

    Added T-Itinerary-Day

---
+++ src/components/T-Itinerary-Day/T-Itinerary-Day.vue
@@ -0,0 +1,34 @@
+<script setup>
+import {onMounted} from "vue";
+
+const props = defineProps({
+  date: {
+    type: String,
+    required: true
+  },
+  day: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  }
+})
+
+</script>
+
+<template>
+  <section>
+    <header class="grid grid-cols-6 pb-2">
+      <time datetime="2024-10-10" class="col-span-1 font-medium text-xl">{{ date }}</time>
+      <h2 class="col-span-3 text-left font-medium text-xl">{{ day }}</h2>
+      <p v-html="description" class="col-span-1 text-right font-medium text-xl"></p>
+    </header>
+    <div class="grid grid-cols-6">
+      <ul class="grid grid-cols-1 gap-4 col-start-2 col-span-4">
+        <slot></slot>
+      </ul>
+    </div>
+  </section>
+</template>
---
+++ src/components/T-Itinerary-Day/T-Itinerary-Flight.vue
@@ -0,0 +1,60 @@
+<script setup>
+
+defineProps({
+  flight: {
+    type: Object,
+    required: true
+  }
+})
+
+</script>
+
+<template>
+  <li class="px-3 py-2 flex justify-between items-start bg-white md:shadow-[0_20px_10px_-15px_rgba(0,110,184,0.08)] rounded-xl">
+    <div >
+      <div class="font-semibold text-brand-blue pt-1">
+        {{ flight.flightNumber }}
+      </div>
+      <p class="text-sm break-words break-after-all pr-3">
+        {{ flight.airline }}
+      </p>
+    </div>
+    <div class="flex flex-col w-[80%] ">
+      <div class="flex items-center h-full py-1.5 w-full">
+        <div class="mx-auto rounded-full border-neutral-200 border-3 h-4 w-4"></div>
+        <hr class="mx-auto h-0 w-full border border-neutral-200 border-dashed"/>
+        <div class="mx-auto rounded-full border-black border-3 h-4 w-4"></div>
+      </div>
+
+      <div class="flex justify-between text-base w-full font-semibold">
+        <p>
+          {{ flight.departureCity }}
+          <time class="font-medium text-sm">
+            ({{ flight.departureTime }})
+          </time>
+        </p>
+        <p class="text-sm font-light">
+          {{ flight.duration }}
+        </p>
+        <p>
+          {{ flight.arrivalCity }}
+          <time class="font-medium text-sm">
+            ({{ flight.arrivalTime }})
+          </time>
+        </p>
+      </div>
+
+      <div class="text-sm text-neutral-400 w-full flex flex-row pb-2">
+        <p>{{ flight.aircraft }}</p>
+      </div>
+
+    </div>
+
+  </li>
+</template>
+
+<style>
+  .border-3 {
+    border-width: 3px;
+  }
+</style>
